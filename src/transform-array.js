c5f7f316d944b3f33c95f649a08a28ba25d5e1fe
Alex <qatechnix@gmail.com>
2020-10-20 17:00:03 +0700

    feat: add transform array task solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (Array.isArray(arr) != 1) {
+    throw new error;
+  } else {
+    let newArr = arr.slice();
+    for (let i = 0; i < newArr.length; i++) {
+      if (typeof newArr[i] === 'string') {
+        switch (newArr[i]) {
+          case '--discard-next':
+            newArr[i + 1] = 'delete';
+            newArr[i] = 'delete'
+            i++;
+            break;
+          case '--discard-prev':
+            newArr[i - 1] = 'delete';
+            newArr[i] = 'delete'
+            break;
+          case '--double-next':
+            newArr[i] = newArr[i + 1];
+            break;
+          case '--double-prev':
+            newArr[i] = newArr[i - 1];
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    return newArr.filter(word => word != 'delete' && word != undefined)
+  }
 };
